587af94ff292e356bce2a6b0c182d13b1ad4b04e
duanhaochen <duanhaochen@126.com>
2017-09-26 14:26:19 +0800

    降低babel-core版本为7.0.0-beta.0

--- config/babel.config.js
+++ config/babel.config.js
@@ -25,7 +25,7 @@ module.exports = {
         'env',
         {
           targets: {      // 兼容列表：https://cloud.githubusercontent.com/assets/588473/19214029/58deebce-8d48-11e6-9004-ee3fbcb75d8b.png
-            // ie: 11,    // 兼容Ie：     v11(!bug)
+            ie: 11,       // 兼容IE：     v11(!bug)
             edge: 12,     // 兼容Edge：   v12
             chrome: 40,   // 兼容Chrome： v40
             firefox: 40   // 兼容Firefox：v40
