9323f889cf3a66c68c9fa9449b154ebb60b0c6dd
duanhaochen <duanhaochen@126.com>
2017-11-01 14:11:57 +0800

    修改路由

--- src/app.js
+++ src/app.js
@@ -2,28 +2,20 @@
 import 'core-js/es6/promise';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, withRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import './common.sass';
 import Layout from './components/Layout';
 
-/* 判断是否需要登录 */
-@withRouter
-class App extends Component{
-  render(): Object{
-    const { pathname }: { pathname: string } = this.props.location; // 判断登录渲染
-    return !/^\/login$/ig.test(pathname) ?
-      (<Layout />) :
-      (<div>登录</div>);
-  }
-}
-
 /* app */
 ReactDOM.render(
   <Provider store={ store }>
     <BrowserRouter>
-      <App />
+      <Switch>
+        <Route path="/Login" component={ (props: Object): Object=>(<div>登录</div>) } exact />
+        <Route component={ Layout } exact />
+      </Switch>
     </BrowserRouter>
   </Provider>,
   document.getElementById('react-app')
