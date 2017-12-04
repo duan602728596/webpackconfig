077a33806101b5cbfebb6332454631967e6054e9
duanhaochen <duanhaochen@126.com>
2017-12-04 23:23:30 +0800

    升级antd到3.0.0

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { LocaleProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
 import store from './store/store';
 import './common.sass';
 import Layout from './assembly/Layout';
@@ -10,12 +12,14 @@ import Layout from './assembly/Layout';
 /* app */
 ReactDOM.render(
   <Provider store={ store }>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/Login" component={ (props: Object): Object=>(<div>登录</div>) } exact />
-        <Route component={ Layout } exact />
-      </Switch>
-    </BrowserRouter>
+    <LocaleProvider locale={ zhCN }>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/Login" component={ (props: Object): Object=>(<div>登录</div>) } exact />
+          <Route component={ Layout } exact />
+        </Switch>
+      </BrowserRouter>
+    </LocaleProvider>
   </Provider>,
   document.getElementById('react-app')
 );
