44a477bc583b54f58b8a0ac95f87908681bc60bf
duanhaochen <duanhaochen@126.com>
2018-01-15 14:55:32 +0800

    修改热替换配置

--- config/babel.config.js
+++ config/babel.config.js
@@ -16,42 +16,48 @@ function output(env){
 const env = process.env.NODE_ENV;
 const debug = output(env);
 
+const presets = [
+  [
+    '@babel/preset-env',
+    {
+      targets: {
+        ie: 11,
+        edge: 12,
+        chrome: 40,
+        firefox: 40
+      },
+      debug: debug,
+      modules: false,
+      useBuiltIns: 'usage',
+      uglify: false
+    }
+  ],
+  '@babel/preset-flow',
+  '@babel/preset-react'
+];
+
+const plugins = [
+  '@babel/plugin-proposal-decorators',
+  '@babel/plugin-proposal-export-default-from',
+  '@babel/plugin-proposal-object-rest-spread',
+  [
+    'import',
+    {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: 'css'
+    }
+  ]
+];
+
+// 热替换插件
+if(debug) plugins.unshift('react-hot-loader/babel');
+
 module.exports = {
   path: 'babel-loader',
   query: {
     cacheDirectory: true,
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            ie: 11,
-            edge: 12,
-            chrome: 40,
-            firefox: 40
-          },
-          debug: debug,
-          modules: false,
-          useBuiltIns: 'usage',
-          uglify: false
-        }
-      ],
-      '@babel/preset-flow',
-      '@babel/preset-react'
-    ],
-    plugins: [
-      'react-hot-loader/babel',
-      '@babel/plugin-proposal-decorators',
-      '@babel/plugin-proposal-export-default-from',
-      '@babel/plugin-proposal-object-rest-spread',
-      [
-        'import',
-        {
-          libraryName: 'antd',
-          libraryDirectory: 'es',
-          style: 'css'
-        }
-      ]
-    ]
+    presets,
+    plugins
   }
 };
