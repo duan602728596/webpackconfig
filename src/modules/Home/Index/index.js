16d3082b7a1d3686ef802bc2012cce980472cda5
duanhaochen <duanhaochen@126.com>
2018-01-18 09:35:43 +0800

    更新依赖包

--- src/modules/Home/Index/index.js
+++ src/modules/Home/Index/index.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Row, Col } from 'antd';
 import style from './style.sass';
 import ListDisplay from './listDisplay';
-import Chart1 from './chart1';
-import Chart2 from './chart2';
 
 class Index extends Component{
   render(): Object{
@@ -16,18 +14,10 @@ class Index extends Component{
         {/* 右侧显示图表 */}
         <Col xs={ 24 } sm={ 24 } md={ 12 } lg={ 14 } xl={ 14 }>
           <Row type="flex" gutter={ 10 }>
-            <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>
-              <Chart1 />
-            </Col>
-            <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>
-              <Chart2 />
-            </Col>
-            <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>
-              <Chart1 />
-            </Col>
-            <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>
-              <Chart2 />
-            </Col>
+            <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>图表</Col>
+            <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>图表</Col>
+            <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>图表</Col>
+            <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>图表</Col>
           </Row>
         </Col>
       </Row>
