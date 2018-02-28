e562b713eee0beef43bca068dfa9eeb76d2635bb
duanhaochen <duanhaochen@126.com>
2018-02-28 13:55:48 +0800

    antd-themes

---
+++ less.config.js
@@ -0,0 +1,18 @@
+/* less-loader 配置 (for antd) */
+
+// 换肤
+// https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+const modifyVars = {
+  // -------- Colors -----------
+  '@primary-color': '#0099bc',
+  // Layout
+  '@layout-body-background': '#fff',
+  '@layout-header-background': '@primary-color'
+};
+
+module.exports = {
+  loader: 'less-loader',
+  options: {
+    modifyVars
+  }
+};
