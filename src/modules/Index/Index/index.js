9b4d906d8e714b8717f423feba3a56ebb9b4f2ba
段昊辰 <duanhaochen@126.com>
2018-07-16 16:12:36 +0800

    修改配置，调整命令

--- src/modules/Index/Index/index.js
+++ src/modules/Index/Index/index.js
@@ -14,7 +14,7 @@ class Index extends Component{
         {/* 右侧显示图表 */}
         <Col xs={ 24 } sm={ 24 } md={ 12 } lg={ 14 } xl={ 14 }>
           <Row type="flex" gutter={ 10 }>
-            <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>图表</Col>
+            <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>图表2</Col>
             <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>图表</Col>
             <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>图表</Col>
             <Col xs={ 24 } sm={ 12 } md={ 24 } lg={ 12 } xl={ 12 } className={ style.mb10 }>图表</Col>
