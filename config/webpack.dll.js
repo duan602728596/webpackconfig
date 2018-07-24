92d1371c2decffa7b294a9700896829a2f4267e5
duan602728596 <duanhaochen@126.com>
2018-07-24 17:36:26 +0800

    调整webpack的dll配置

--- config/webpack.dll.js
+++ config/webpack.dll.js
@@ -65,6 +65,7 @@ module.exports = {
       path: '.dll/manifest.json',
       name: '[name]_[hash]',
       context: __dirname
-    })
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
   ]
 };
