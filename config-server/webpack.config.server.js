6e97fd809acf6574f30924d44c3a041261088734
duan602728596 <duanhaochen@126.com>
2018-08-22 09:44:00 +0800

    更新依赖，修改服务器端的配置

--- config-server/webpack.config.server.js
+++ config-server/webpack.config.server.js
@@ -55,7 +55,8 @@ function config(){
                   loader: 'file-loader',
                   options: {
                     name: fileName,
-                    outputPath: 'image/'
+                    outputPath: 'image/',
+                    emitFile: false
                   }
                 }
               }
@@ -69,7 +70,8 @@ function config(){
               loader: 'file-loader',
               options: {
                 name: fileName,
-                outputPath: 'file/'
+                outputPath: 'file/',
+                emitFile: false
               }
             }
           ]
