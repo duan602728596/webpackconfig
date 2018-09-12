9391302ea6781acc7f4ce765b4c568ae217cb899
段昊辰 <duanhaochen@126.com>
2018-09-12 23:46:10 +0800

    添加initial state

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import Vue from 'vue/dist/vue';
 import './iview';
 import App from './AppModule.vue';
-import store from './store/store';
+import { storeFactory } from './store/store';
 import routers from './router/routers';
 import './common.sass';
 
@@ -9,7 +9,7 @@ import './common.sass';
 /* app */
 const app: Vue = new Vue({
   el: '#vue-app',
-  store,
+  store: storeFactory(window.__INITIAL_STATE__ || {}),
   router: routers,
   components: {
     App
--- src/store/store.js
+++ src/store/store.js
@@ -4,10 +4,22 @@ import index from '../modules/Index/store/index';
 
 Vue.use(Vuex);
 
-const store: Vuex.Store = new Vuex.Store({
-  modules: {
-    index
-  }
-});
+const store: Vuex.Store = {};
+
+export function storeFactory(initialState: ?Object): Object{
+  /* store */
+  Object.assign(store, new Vuex.Store({
+    state: initialState,
+    getters: {
+      getInitialStateData: (state: Object): Function => (key: string): number => state[key]
+    },
+    modules: {
+      index
+    }
+  }));
+
+  return store;
+}
+
 
 export default store;
